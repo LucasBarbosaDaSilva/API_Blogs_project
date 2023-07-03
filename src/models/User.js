aaf5fc9922b6688660cfe4c868ef78ed6bd300fe
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-07-03 16:00:13 -0300

    Criando model de User

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'users',
+    timestamps: false,
+    underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'user_id',
+      as: 'blog_posts',
+    });
+  };
+
+  return User;
+}
