3e03f22d20e24a83b16299adce8be29ef15987f0
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-07-03 15:45:48 -0300

    Criando migrations, req 1

---
+++ src/migrations/20230703182100-blog_posts.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      title:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      content:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      published:{
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated:{
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      user_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
