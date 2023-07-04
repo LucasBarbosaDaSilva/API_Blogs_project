21aeb4b4f30488bd9242e349bee46546a9ed824c
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-07-03 22:20:23 -0300

    Criando model Category

---
+++ src/models/Category.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING,
+  }, {
+    tableName: 'categories',
+    timestamps: false,
+    underscored: true,
+  });
+return Category;
+};
